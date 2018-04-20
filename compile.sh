79ec98280434c1fc9ef259d1693e0ed43a9500ab
Zhenguo <chrisyin.2vv@gmail.com>
2018-04-20 15:34:18 -0400

    add cpmpile script and config file

---
+++ compile.sh
@@ -0,0 +1,7 @@
+make distclean
+KERNEL=kernel7
+#make ARCH=arm CROSS_COMPILE=arm-eabi- bcm2709_defconfig
+make ARCH=arm CROSS_COMPILE=arm-eabi- ckos_zimage_defconfig
+#make ARCH=arm CROSS_COMPILE=arm-eabi- menuconfig
+make CFLAGS=-g ARCH=arm CROSS_COMPILE=arm-eabi- zImage -j8
+cp ${PWD}/arch/arm/boot/zImage $tftp_path/pi3/img/zImage
