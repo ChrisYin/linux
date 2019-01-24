528aa903c94f54abae0d471730bcde8bb5380a12
Zhenguo <chrisyin.2vv@gmail.com>
2019-01-24 14:15:05 -0500

    modifed configs and compile scripts

--- ccsd.sh
+++ ccsd.sh
@@ -1,6 +1,15 @@
+mkdir mnt
+mkdir mnt/fat32
+mkdir mnt/ext4
+sudo mount /dev/sdc1 mnt/fat32
+sudo mount /dev/sdc2 mnt/ext4
+
 sudo make CFLAGS=-g ARCH=arm CROSS_COMPILE=arm-eabi- INSTALL_MOD_PATH=mnt/ext4 modules_install
-cp mnt/fat32/$KERNEL.img mnt/fat32/$KERNEL-backup.img
-cp arch/arm/boot/zImage mnt/fat32/$KERNEL.img
-cp arch/arm/boot/dts/*.dtb mnt/fat32/
-cp arch/arm/boot/dts/overlays/*.dtb* mnt/fat32/overlays/
-cp arch/arm/boot/dts/overlays/README mnt/fat32/overlays/
+sudo cp mnt/fat32/$KERNEL.img mnt/fat32/$KERNEL-backup.img
+sudo cp arch/arm/boot/zImage mnt/fat32/$KERNEL.img
+sudo cp arch/arm/boot/dts/*.dtb mnt/fat32/
+sudo cp arch/arm/boot/dts/overlays/*.dtb* mnt/fat32/overlays/
+sudo cp arch/arm/boot/dts/overlays/README mnt/fat32/overlays/
+
+sudo umount mnt/fat32
+sudo umount mnt/ext4
--- compile.sh
+++ compile.sh
@@ -1,9 +1,10 @@
-make distclean
+#make distclean
 KERNEL=kernel7
 #make ARCH=arm CROSS_COMPILE=arm-eabi- bcm2709_defconfig
-make ARCH=arm CROSS_COMPILE=arm-eabi- ckos_zimage_defconfig
+make ARCH=arm CROSS_COMPILE=arm-eabi- ckos_smp_defconfig
+#make ARCH=arm CROSS_COMPILE=arm-eabi- ckos_smp_usb_defconfig
 #make ARCH=arm CROSS_COMPILE=arm-eabi- menuconfig
 make CFLAGS=-g ARCH=arm CROSS_COMPILE=arm-eabi- zImage -j8
-#make CFLAGS=-g ARCH=arm CROSS_COMPILE=arm-eabi- modules -j8
-#make CFLAGS=-g ARCH=arm CROSS_COMPILE=arm-eabi- dtbs -j8
-#cp ${PWD}/arch/arm/boot/zImage $tftp_path/pi3/img/zImage
+make CFLAGS=-g ARCH=arm CROSS_COMPILE=arm-eabi- modules -j8
+make CFLAGS=-g ARCH=arm CROSS_COMPILE=arm-eabi- dtbs -j8
+cp ${PWD}/arch/arm/boot/zImage $tftp_path/pi3/img/zImage
